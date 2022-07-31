d1605171457098898fe6e581a96296723fe1cfec
Andrii Ferenchuk <ferenchuk.a@gmail.com>
2022-07-31 22:45:06 +0300

    feat: implement functions for buttons

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-let homeScore = document.getElementById("home").textContent;
+function addOneHome() {
+  document.getElementById("home").textContent =
+    +document.getElementById("home").textContent + 1;
+}
 
-function addOne() {
-  homeScore++;
+function addTwoHome() {
+  document.getElementById("home").textContent =
+    +document.getElementById("home").textContent + 2;
+}
+
+function addThreeHome() {
+  document.getElementById("home").textContent =
+    +document.getElementById("home").textContent + 3;
+}
+
+function addOneGuest() {
+  document.getElementById("guest").textContent =
+    +document.getElementById("guest").textContent + 1;
+}
+
+function addTwoGuest() {
+  document.getElementById("guest").textContent =
+    +document.getElementById("guest").textContent + 2;
+}
+
+function addThreeGuest() {
+  document.getElementById("guest").textContent =
+    +document.getElementById("guest").textContent + 3;
 }
